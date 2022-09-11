ddda77d41f61ec0a48206553a5009ff21e41148e
amvmail <amv_mail@mail.ru>
2022-09-11 12:59:50 +0300

    commit for review

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 
 from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                     ReviewViewSet, TitleViewSet, UserViewSet, get_jwt_token,
                     signup)
+from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('titles', TitleViewSet)
