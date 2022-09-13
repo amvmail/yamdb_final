2d61ec7925e9ba5f14160c0e3196b1922f507578
and_rem <C:\Users\\AppData\Roaming\The Bat!>
2022-09-13 14:18:35 +0300

    review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,18 +1,14 @@
 import os
 from datetime import timedelta
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # сейчас без  явного указания ключа в файле тесты не проходят
 SECRET_KEY = os.getenv('SECRET_KEY',
                        default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
-DEBUG = os.getenv('DEBUG')
+# сейчас без  явного указания режима debug файле тесты не проходят
+DEBUG = 'false'
 
 ALLOWED_HOSTS = ['*']
 
