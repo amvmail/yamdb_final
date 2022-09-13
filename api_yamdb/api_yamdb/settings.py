04721d5104ebd9fd165a56fe49f4e5fc853261b9
amvmail <amv_mail@mail.ru>
2022-09-13 13:34:25 +0300

    review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,6 +3,8 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 DEBUG = os.getenv('DEBUG', default='false')
 
 ALLOWED_HOSTS = ['*']
